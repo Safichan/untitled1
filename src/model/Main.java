9cf1c62284885dd86b0347723e9c7b3884e60de5
Mary Read <jungautor@web.de>
2022-07-10 12:57:21 +0200

    added things in Gui.java

--- src/model/Main.java
+++ src/model/Main.java
@@ -5,7 +5,6 @@ import userinterface.Gui;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         Gui test = new Gui();
     }
 
--- src/userinterface/Gui.java
+++ src/userinterface/Gui.java
@@ -2,20 +2,27 @@ package userinterface;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.security.cert.TrustAnchor;
+import java.awt.event.*;
 
 /**
  * userinterface.Gui created a GUI to get an overview over trainers and pokemon. Currently in progress.
  */
-public class Gui implements ActionListener {
+public class Gui implements ActionListener, ItemListener {
 
     // all components
     protected JFrame frame;
+
     protected JPanel contentPane;
-    protected JButton button1;
-    protected JComboBox trainer;
+    protected JPanel buttonPanel;
+    protected JPanel comboPanel;
+
+    protected JLabel comboLabel;
+
+    protected JButton butNew;
+    protected JButton butEdit;
+    protected JButton butDel;
+
+    protected JComboBox<String> trainer;
     protected String[] trainerList = {"du da", "er da", "Noir", "Han", "Mary", "ich", "du"};
 
     /**
@@ -29,18 +36,40 @@ public class Gui implements ActionListener {
         this.frame.setContentPane(contentPane);
 
         // adding elements
-        this.button1 = new JButton("Test");
-        this.button1.setBounds(130, 100, 100, 40);
-        this.button1.addActionListener(this);
-        this.contentPane.add(button1);
+        this.comboPanel = new JPanel();
 
-    /*this.trainer = new JComboBox(trainerList);
-    trainer.setSize(200, 20);
-    this.contentPane.add(trainer);*/
+        this.trainer = new JComboBox<String>(trainerList);
+        trainer.addItemListener(this);
+        trainer.setSize(100, 30);
+        this.comboPanel.add(trainer, BorderLayout.CENTER);
+        this.comboLabel = new JLabel(" ");
+        this.comboPanel.add(comboLabel);
+
+        //buttons
+        this.buttonPanel = new JPanel();
+
+        this.butNew = new JButton("New");
+        this.butNew.setBounds(130, 100, 100, 40);
+        this.butNew.addActionListener(this);
+        this.buttonPanel.add(butNew);
+
+        this.butEdit = new JButton("Edit");
+        this.butEdit.setBounds(130, 100, 100, 40);
+        this.butEdit.addActionListener(this);
+        this.buttonPanel.add(butEdit);
+
+        this.butDel = new JButton("Delete");
+        this.butDel.setBounds(130, 100, 100, 40);
+        this.butDel.addActionListener(this);
+        this.buttonPanel.add(butDel);
+
+        this.contentPane.add(comboPanel);
+        this.contentPane.add(buttonPanel);
 
         // set Layout
-        this.frame.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
-        this.frame.setSize(300, 400);
+        this.frame.setLayout(new FlowLayout());
+        this.frame.setSize(300, 200);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // set visible
         this.frame.setVisible(true);
@@ -53,8 +82,21 @@ public class Gui implements ActionListener {
      */
     @Override
     public void actionPerformed(ActionEvent ae) {
-        if (ae.getSource() == button1) {
-            System.out.println("Hallo!! Button 1 gedrueckt!");
+        if (ae.getSource() == butNew) {
+          System.out.println("Neuer Trainer wird erstellt!");
+        }
+        if (ae.getSource() == butEdit) {
+          System.out.println("Trainer wird bearbeitet!");
+        }
+        if (ae.getSource() == butDel) {
+          System.out.println("Trainer wird gelöscht!");
+        }
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent ie) {
+        if (ie.getSource() == trainer) {
+            comboLabel.setText(trainer.getSelectedItem() + " selected");
         }
     }
 
